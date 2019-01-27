4173757d5f56653785f5f015535dab9933da2a50
Duncan McGreggor <oubiwann@gmail.com>
2019-01-27 12:41:24 -0500

    Added workaround for JDK11 and rrb-vector.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject venantius/ultra "0.5.3"
+(defproject venantius/ultra "0.5.4"
   :description "Ultra: A Leiningen plugin for a superior development environment"
   :url "http://github.com/venantius/ultra"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-
+  :exclusions [[org.clojure/core.rrb-vector]]
+  :dependencies [[org.clojure/core.rrb-vector "0.0.13"]
+                 [org.clojure/tools.nrepl "0.2.12"]
                  [grimradical/clj-semver "0.3.0" :exclusions [org.clojure/clojure]]
                  [io.aviso/pretty "0.1.30"]
                  [mvxcvi/whidbey "1.3.0"]
